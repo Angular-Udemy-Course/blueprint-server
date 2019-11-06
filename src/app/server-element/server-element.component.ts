219a5c19b9a2736d6dfaa09d196230cd0e4be6cd
golkedj <golkedj@gmail.com>
2019-11-06 16:40:12 -0700

    See ContentChild in lifecycle hooks

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -10,7 +10,8 @@ import {
   AfterViewChecked,
   OnDestroy,
   ViewChild,
-  ElementRef
+  ElementRef,
+  ContentChild
 } from '@angular/core';
 
 @Component({
@@ -29,6 +30,7 @@ export class ServerElementComponent implements
   @Input('srvElement') element: {type: string, name: string, content: string};
   @Input() name: string;
   @ViewChild('heading', { static: true }) header: ElementRef;
+  @ContentChild('contentParagraph', { static: true }) paragraph: ElementRef;
 
   constructor() {
     console.log('constructor called!');
@@ -42,6 +44,7 @@ export class ServerElementComponent implements
   ngOnInit() {
     console.log('ngOnInit called!');
     console.log(`Text Content: "${this.header.nativeElement.textContent}"`);
+    console.log(`Text Content of paragraph: "${this.paragraph.nativeElement.textContent}"`);
   }
 
   ngDoCheck(): void {
@@ -50,6 +53,7 @@ export class ServerElementComponent implements
 
   ngAfterContentInit(): void {
     console.log('ngAFterContentInit called!');
+    console.log(`Text Content of paragraph: "${this.paragraph.nativeElement.textContent}"`);
   }
   
   ngAfterViewInit(): void {
